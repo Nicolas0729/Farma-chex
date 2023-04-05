8e37470b494353566b33911655508ea842ba31f9
Nicolas0729 <107268101+Nicolas0729@users.noreply.github.com>
2023-04-05 12:21:58 -0500

    cambios

---
+++ js/carousel-logos.js
@@ -0,0 +1,33 @@
+$('.carousel-logos').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout:2000,
+    margin: 20,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        300:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        900:{
+            items: 3,
+            nav: false
+        },
+        1140:{
+            items: 5.5,
+            nav: false
+        },
+        1340:{
+            items: 5.5,
+            nav: false
+        }
+    }    
+})
